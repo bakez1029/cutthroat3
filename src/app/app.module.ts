ea5251153524598033b5c20db006f11cbbdbbd5f
Tom Baker <tobaker@younglivingcom>
2017-01-29 09:28:17 -0700

    basic home page layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,50 @@
+import 'hammerjs'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material'
 import { AppComponent } from './app.component';
-import 'hammerjs'
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PhotosComponent } from './photos/photos.component';
+import { ProductsComponent } from './products/products.component';
+import { BarbersComponent } from './barbers/barbers.component';
+import { ServicesComponent } from './services/services.component';
+import { AdminComponent } from './admin/admin.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PhotosComponent,
+    ProductsComponent,
+    BarbersComponent,
+    ServicesComponent,
+    AdminComponent,
+    JobsComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'barbers', component: BarbersComponent },
+      { path: 'services', component: ServicesComponent },
+      { path: 'photos', component: PhotosComponent },
+      { path: 'jobs', component: JobsComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'login', component: LoginComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
