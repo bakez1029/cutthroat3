0ba6520288b2a42446a621b3195487d68b4c1908
djHelpyFresh <bakez1029@gmail.com>
2017-02-22 16:21:15 -0700

    Fixed admin - u have to be logged in to see admin section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,41 @@ import { AuthService } from './auth.service'
 })
 export class AppComponent implements OnInit {
   authed: any;
+  uid: string;
+  admin: boolean;
+  loggedIn: boolean = false;
+
 
   constructor(public af: AngularFire, private authService: AuthService) { }
 
 
 
   ngOnInit() {
-    console.log("HAPPY BIRTHDAY");
     // we subscribe here so the users auth will get called as soon as a user loads the website
     // this way there won't be a delay in lookuping up the users auth later
     this.af.auth.subscribe(auth => {
       this.authed = auth;
-     
-
+      if (auth && this.authed.uid == "GoObJLWJ23gMGQOwaqNkyQfTiXn1") {
+        this.admin = true
+        this.loggedIn = true;
+        console.log("HELLO ADMIN")
+      } else {
+        console.log("NOT AN ADMIN")
+        this.admin = false
+      }
 
     });
   }
-  
+  logout() {
+    this.authService.logout();
+    console.log('logged out');
+    this.uid = "";
+    this.loggedIn = false;  
+
+  }
+
 }
+
 
 
 
